b33d627a5049c8cdad23afbb30693a419f415b15
bobismijnnaam <bobrubbens@gmail.com>
2017-11-06 15:09:29 +0100

    Finished 53

--- 53/53.py
+++ 53/53.py
@@ -2,6 +2,21 @@
 
 from Utils import *
 
+def factorial(n):
+    if n == 0: return 1
+    return n * factorial(n - 1)
+
+def nCr(n, r):
+    assert(r <= n)
+    return factorial(n) / (factorial(r) * factorial(n - r))
+
 if __name__ == "__main__":
     # Your code here!
-
+    count = 0
+    for n in range(1, 100 + 1):
+        print("Checking", n)
+        print("Progress", count)
+        for r in range(1, n + 1):
+            if nCr(n, r) > 1000000:
+                count += 1
+    print("Count", count)
