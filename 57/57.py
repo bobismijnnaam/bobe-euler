8e788a01e001ad058861ce690d60b7d6da204b37
bobismijnnaam <bobrubbens@gmail.com>
2017-11-07 11:16:30 +0100

    Did 57!

--- 57/57.py
+++ 57/57.py
@@ -2,5 +2,30 @@
 
 from Utils import *
 
+def calculateForSteps(numIterations):
+    assert(numIterations > 0)
+
+    num = (1, 2)
+    for i in range(numIterations - 1):
+        num = (num[0] + num[1] * 2, num[1])
+        num = (num[1], num[0])
+
+    return (num[0] + num[1], num[1])
+
 if __name__ == "__main__":
     # Your code here!
+    print(calculateForSteps(1))
+    print(calculateForSteps(2))
+    print(calculateForSteps(3))
+    print(calculateForSteps(4))
+
+    count = 0
+    for i in range(1, 1000 + 1):
+        res = calculateForSteps(i)
+        numerator = res[0]
+        denominator = res[1]
+
+        if len(str(numerator)) > len(str(denominator)):
+            count += 1
+
+    print("Count:", count)
