39db9e7bde823adae14358f664d3614028fa27a3
bobismijnnaam <bobrubbens@gmail.com>
2017-11-07 10:55:11 +0100

    Finished 56!

--- 56/56.py
+++ 56/56.py
@@ -4,3 +4,17 @@ from Utils import *
 
 if __name__ == "__main__":
     # Your code here!
+
+    maxSum = -1
+    for a in range(100):
+        print("a =", a)
+        for b in range(100):
+            num = BigInt()
+            num.add(a)
+            num.pow(b)
+
+            digitSum = sum([int(x) for x in list(num.toString())])
+            if digitSum > maxSum:
+                maxSum = digitSum
+
+    print("Max", maxSum)
